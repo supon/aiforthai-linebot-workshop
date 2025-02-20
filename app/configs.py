3dedbcfd7f507cadabbc313e3e3880d684695de7
supon <suponk@yahoo.com>
2025-02-20 09:33:47 +0700

    Configuration file with varable for NLP

--- app/configs.py
+++ app/configs.py
@@ -1,7 +1,16 @@
 from pydantic_settings import BaseSettings
 
-
 class Configs(BaseSettings):
     AIFORTHAI_APIKEY: str
     LINE_CHANNEL_ACCESS_TOKEN: str
-    LINE_CHANNEL_SECRET:str      
+    LINE_CHANNEL_SECRET: str
+    URL: str
+    WAV_FILE: str
+    DIR_FILE: str
+    URL_VAJA: str
+    URL_PARTII: str
+
+    class Config:
+        env_file = ".env"  # Ensure it reads from .env file
+
+ 
